2f7e38d234e519ed06418466e8f278d70a86265c
ck <ck1956@nyu.edu>
2016-12-05 21:30:35 -0500

    initial

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = "3Eh_4KojAoJLVmrHQLyX";
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Stock Ticker"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <input
+          placeholder="Start Date in yyyy-mm-dd format"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <input
+          placeholder="End Date in yyyy-mm-dd format"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
